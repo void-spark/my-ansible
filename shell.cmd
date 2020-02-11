c5b4529a215b149ad0453781806a35ce3c43ea53
void-spark <81029971+void-spark@users.noreply.github.com>
2020-02-11 18:47:57 +0100

    Change delete to inventory based as a PoC Fix cmd, docker doesn't understand '.' anymore

--- shell.cmd
+++ shell.cmd
@@ -1,5 +1,4 @@
 @ECHO OFF
-docker run -it --rm -v "%~dp0.:/work" -v "%userprofile%/.ssh:/tmp/.ssh:ro" -w /work void-spark/ansible
-
+docker run -it --rm -v "%~dp0:/work" -v "%userprofile%/.ssh:/tmp/.ssh:ro" -w /work void-spark/ansible
 
 REM docker run -it --rm -v "%~dp0..:/esp/project" -w /esp/project void-spark/ansible bash -c "make -j8 all"

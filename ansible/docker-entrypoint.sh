5173b1e16d3d48414cb1cb771ad5702635fc11da
void-spark <81029971+void-spark@users.noreply.github.com>
2022-02-12 12:02:38 +0000

    We're on linux now

--- ansible/docker-entrypoint.sh
+++ ansible/docker-entrypoint.sh
@@ -6,4 +6,8 @@ if [ -d "/tmp/.ssh" ]; then
     chmod -R 600 /root/.ssh/*
 fi
 
+echo "[user]" > /root/.gitconfig
+echo "        name = void-spark" >> /root/.gitconfig
+echo "        email = 81029971+void-spark@users.noreply.github.com" >> /root/.gitconfig
+
 exec "$@"

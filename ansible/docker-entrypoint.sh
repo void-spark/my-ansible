64a7462f093764ee5d45109db58272003a6fd193
void-spark <81029971+void-spark@users.noreply.github.com>
2024-05-18 18:51:17 +0000

    Update Ansible container

--- ansible/docker-entrypoint.sh
+++ ansible/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 set -e
 
 if [ -d "/tmp/.ssh" ]; then
-    cp -R /tmp/.ssh /home/vscode/
-    chmod -R 600 /home/vscode/.ssh/*
+    cp -R /tmp/.ssh /home/ubuntu/
+    chmod -R 600 /home/ubuntu/.ssh/*
 fi
 
-echo "[user]" > /home/vscode/.gitconfig
-echo "        name = void-spark" >> /home/vscode/.gitconfig
-echo "        email = 81029971+void-spark@users.noreply.github.com" >> /home/vscode/.gitconfig
+echo "[user]" > /home/ubuntu/.gitconfig
+echo "        name = void-spark" >> /home/ubuntu/.gitconfig
+echo "        email = 81029971+void-spark@users.noreply.github.com" >> /home/ubuntu/.gitconfig
 
 exec "$@"
